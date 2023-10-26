8e40c441a9e0319082668efee2d22ebdf3aad707
jung sungho <openwater301@hanmail.net>
2023-10-26 09:48:36 +0900

    path modify

--- js/fetch.js
+++ js/fetch.js
@@ -1,12 +1,11 @@
-$(function(){ alert("jquery ok!"); //jquery시작
-  //코드~
+$(function(){ alert("jquery ok!"); 
 
-  let clickedCount = 1; //초기값
+  let clickedCount = 1; 
 
-      let cart = []; //장바구니
-      let totalSum = 0; //장바구니에 담긴 모든합계
+      let cart = []; 
+      let totalSum = 0; 
    
-      $(".order1").click(function(){//주문하기 페이지보기
+      $(".order1").click(function(){
           document.getElementById("step1").style.display = "none";
           document.getElementById("step2").style.display = "none";
           document.getElementById("step3").style.display = "none";
@@ -14,14 +13,14 @@ $(function(){ alert("jquery ok!"); //jquery시작
       });
 
 
-  $(".goCart").click(function(){//step3가기
+  $(".goCart").click(function(){
     document.getElementById("step1").style.display = "none";
     document.getElementById("step2").style.display = "none";
     document.getElementById("step3").style.display = "block";
     document.getElementById("step4").style.display = "none";    
   });
 
-  $(".gotoStep1").click(function(){//step1가기
+  $(".gotoStep1").click(function(){
     document.getElementById("step1").style.display = "block";
     document.getElementById("step2").style.display = "none";
     document.getElementById("step3").style.display = "none";
@@ -29,27 +28,21 @@ $(function(){ alert("jquery ok!"); //jquery시작
   });
 
 function newsList() {
-  alert("newsList함수실행 ok");
 
-  let imgSrc;//경로
-  let imgAlt;//상품명
-  let imgPrice;//가격
+  let imgSrc;
+  let imgAlt;
+  let imgPrice;
 
-  //$("#newsList")
-  //이부분에 json데이터가 보여지기전에 로딩이미지를 보여준다
-
- // setTimeout(() => {
-    fetch("./data/goods.json")
-      .then((response) => response.json()) //json을 객체로 변환
+    fetch("https://openwater301.github.io/testSite/data/goods.json")
+      .then((response) => response.json()) 
       .then((json) => {
-        //객체로 출력
-        let { goods } = json; //비구조화할당
-        alert(goods);//object
-        alert(goods.length);//6   goods배열에 담긴 객체6개확인 
+        let { goods } = json; 
+        alert(goods);
+        alert(goods.length);
 
-        let tableBody = [];//초기화   
-        if(goods.length >0){//goods에 1개이상 데이터가 있을경우  
-            for(var i=0; i<goods.length; i++){//6번 반복
+        let tableBody = []; 
+        if(goods.length >0){
+            for(var i=0; i<goods.length; i++){
                 tableBody +=  "<tr>";
                 tableBody +=  "<td>" + goods[i].title  + "</td>";
                 tableBody +=  "<td><a href='#' class='goods" + i 
@@ -58,22 +51,20 @@ function newsList() {
                                + "' /></a></td>";
                 tableBody +=  "<td>" + goods[i].price  + "</td>";
                  tableBody += "</tr>";  
-            }//for문 종료
+            }
 
-            $("#newsList").append(tableBody);//table에 보내기 
+            $("#newsList").append(tableBody);
             $("#newsList tr").click(function(){
-                //alert("상품클릭ok");
-               imgSrc = $(this).find("img").attr("src");//경로
-               imgAlt = $(this).find("img").attr("alt");//상품명
-               imgPrice = $(this).find("img").attr("title");//가격
+                imgSrc = $(this).find("img").attr("src");
+               imgAlt = $(this).find("img").attr("alt");
+               imgPrice = $(this).find("img").attr("title");
 
-              //alert(imgSrc+imgAlt+imgPrice);//클릭한상품의 정보확인 
               $("#myfood").attr("src",imgSrc);
               $("#myfoodName").text(imgAlt);
               $("#myfoodPrice").text(imgPrice);
 
               $("#step1").css("display","none");
-              $("#step2").css("display","block");//상품스펙보기
+              $("#step2").css("display","block");
 
               if(imgAlt == "상품1"){
                 $(".spec_is").html("<p>상품1은 ~~~~~~~~   </p>");
@@ -91,23 +82,22 @@ function newsList() {
 
             });
 
-        }//if문 종료
+        }
         
-        //수량조정함수
-        $(".plus").click(function(){  //더하기
-            clickedCount += 1; //1더하기
+        $(".plus").click(function(){ 
+            clickedCount += 1; 
             if (clickedCount > 10) {
               alert("10개까지만 주문할 수 있습니다");
-              clickedCount = 10;//alert("더하기");
+              clickedCount = 10;
             } else {
               $(".count").text(clickedCount);
             }
         });
-        $(".minus").click(function(){ //빼기
-            clickedCount -= 1;   //1빼기
+        $(".minus").click(function(){ 
+            clickedCount -= 1;  
             if (clickedCount < 1) {
               alert("1개이상 주문할 수 있습니다");
-              clickedCount = 1;//alert("빼기");
+              clickedCount = 1;
             } else {
               $(".count").text(clickedCount);
             }          
@@ -137,58 +127,48 @@ function newsList() {
               "\n";
           }
         }
-        alert(box);//테스트 
 
-        let num = cart.length - 1;
-        //cart = cart.length;
-        alert("cart.length에서 1을뺀값: " + num);
-
-        
-        let shoppingCart_tbody = " ";//초기화
+        let num = cart.length - 1;        
+        let shoppingCart_tbody = " ";
         if(cart.length >0){
-          //for(var i=0 ; i<cart.length ; i++){//번호 상품이미지 상품명 가격 합계
             shoppingCart_tbody += "<tr><td>" +  (num +1)   + "</td>" ; 
             shoppingCart_tbody += "<td>" + "<p>" + cart[num].name    + "</p>"  
                                   +  "<p><img src='" + cart[num].src + "' width='50' height='50' alt='' /></p>"   + "</td>" ; 
             shoppingCart_tbody += "<td>" + cart[num].price + "</td>";
             shoppingCart_tbody += "<td>" + cart[num].count + "</td>";
             shoppingCart_tbody += "<td>" + cart[num].price * cart[num].count + "원</td>";
-          //}
           $(".shoppingCart_tbody").append(shoppingCart_tbody) 
         }
 
-        //합계가 들어갈영역--------------------
         totalSum += cart[num].price * cart[num].count;
         alert("장바구니 총합계: " + totalSum);
-        $(".total1").text(totalSum); //장바구니 총합계표시
+        $(".total1").text(totalSum); 
 
-        //로컬스토리지에 저장
-        saveToLocal(goods1); //로컬스토리지에 저장하는 함수실행
-      });    //담기함수종료    
+        saveToLocal(goods1);
+      });    
 
-     //로컬스토리지에 저장
       function saveToLocal(todo) {
-        let todos; //로컬스토리지에 객체를 담을 배열
+        let todos; 
         if (localStorage.getItem("todos") === null) {
-          todos = []; //배열초기화
+          todos = []; 
         } else {
           todos = JSON.parse(localStorage.getItem("todos"));
         }
         todos.push(todo);
         localStorage.setItem("todos", JSON.stringify(todos));
-      }//로컬저장함수종료
+      }
   
-      getLocal1();//로컬에 담긴데이터 가져오기 
+      getLocal1();
       function getLocal1(todo) {
-        let todos; //로컬스토리지에 객체를 담을 배열
+        let todos; 
         if (localStorage.getItem("todos") === null) {
-          todos = []; //배열초기화
+          todos = []; 
         } else {
           todos = JSON.parse(localStorage.getItem("todos"));
-          cart = todos; //추가 9.20
+          cart = todos; 
         }
         todos.forEach(function (i) {
-          let f = 0; //변수  상품을 담을때 객체1씩
+          let f = 0; 
           do {
             i.name = todos[f].name;
             i.src = todos[f].src;
@@ -196,7 +176,7 @@ function newsList() {
             i.count = todos[f].count;
             f++;
 
-           let shoppingCart_tbody = " ";//초기화
+           let shoppingCart_tbody = " ";
            if(cart.length >0){
               shoppingCart_tbody += "<tr><td>" +  f  + "</td>" ; 
               shoppingCart_tbody += "<td>" + "<p>" +  i.name   + "</p>"  
@@ -208,28 +188,24 @@ function newsList() {
            }
 
             totalSum += i.price * i.count;
-            //alert("총합계: " + totalSum);
-            $(".total1").text(totalSum); //장바구니 총합계표시
-            $(".pay").text(totalSum);//주문시 총합계표시
+            $(".total1").text(totalSum); 
+            $(".pay").text(totalSum);
           } while (f < todos.length + 1);
         });
-      }//로컬저장함수종료
+      }
 
       });
- // }, 1500); //1.5초후에 json데이터보여지게 동작
+
 }
 
-
-      //삭제함수
-      $(".delCart").click(function(){  alert("비우기 클릭");//
+      $(".delCart").click(function(){  
         if (cart.length >= 1) {
-          //담아놓은상품이 1개이상 있을경우 삭제
           let cleanCart = confirm("장바구니상품들을 삭제하겠습니까?");
           if (cleanCart == 1) {
             $(".shoppingCart_tbody").text(" ");     
             totalSum = 0;
-            $(".total1").text(totalSum); //표시  
-            $(".pay").text(totalSum);//주문시 총합계표시
+            $(".total1").text(totalSum);
+            $(".pay").text(totalSum);
             cart = [];
             alert("삭제후 cart.length: " + cart.length);
             localStorage.setItem("todos", JSON.stringify(cart)); //
@@ -238,10 +214,9 @@ function newsList() {
             alert("취소했습니다");
           }
         } else {
-          //담아놓은상품이 없을경우
           alert("담아놓은상품이 없습니다");
         }     
-      });//비우기함수종료
+      });
 
   newsList();
 });
